9031f5af4b6670fe771bb70e5eeaebb0ab6cab84
atsoul03 <atsoul03@cs.ucy.ac.cy>
2019-11-19 00:24:12 +0200

    basket and minor menu changes

--- js/menu.js
+++ js/menu.js
@@ -6,25 +6,53 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 500);
 });
 
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+
 /*
 $(function() {
 
-    var $sidebar   = $("#large-categories"), 
+    var $sidebar   = $("#basket"), 
         $window    = $(window),
         offset     = $sidebar.offset(),
-        topPadding = 15;
+        topPadding = 1;
+	var lastScrollTop = 0
+		initialposition = offset.top;
 
     $window.scroll(function() {
-        if ($window.scrollTop() > (offset.top -110)) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - (offset.top-110) + topPadding
-            }, "0");
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
+		var top_bar_menu = $("#bar-menu-row").offset().top;
+		var bottom_bar_menu = $("#bar-menu-row").offset().top + $("#bar-menu-row").outerHeight(true);
+		var top_sidebar = offset.top;
+		var bottom_sidebar = offset.top + $sidebar.outerHeight(true);
+		var scrollBottom = $window.scrollTop() + $window.height();
+		var scrollTop = $window.scrollTop();
+		
+		var paddingTop_sidebar = offset.top + parseInt($sidebar.css("marginTop").replace('px', ''));
+		
+		
+		var st = $window.scrollTop();
+		if (st > lastScrollTop){
+			// downscroll code
+			if (scrollTop > (top_sidebar -100) && bottom_sidebar < bottom_bar_menu-100 && scrollBottom > bottom_sidebar) {
+				$sidebar.css('margin-top',$window.scrollTop() - (top_sidebar-100) + topPadding+'px');
+			}
+			else if (scrollTop <= (top_sidebar -100)){
+				$sidebar.css('margin-top', 0+'px');
+			}
+		} else {
+			// upscroll code
+			if (scrollTop > (top_sidebar -100) && scrollTop < (paddingTop_sidebar-100)) {
+				$sidebar.css('margin-top',$window.scrollTop() - (top_sidebar-100) - topPadding+'px');
+			}
+			
+		}
+		lastScrollTop = st;
+		
+        
     });
     
 });
 */
+
