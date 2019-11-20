94e7e24d7ddd3c9ce70715f0f2d8c1b96c40150b
mchara01 <mchara01@cs.ucy.ac.cy>
2019-11-20 21:46:18 +0200

    Images changed in event and contact-us & Reservation made responsive.

--- js/reservation.js
+++ js/reservation.js
@@ -3,3 +3,29 @@ $(document).ready(function() {
 
 	$('#datetimepicker1').datepicker().datepicker("setDate", new Date());
 });
+
+function reservation() {
+	let name = document.getElementById("nameForm").value;
+	let telephone = document.getElementById("telForm").value;
+	let email = document.getElementById("emailForm").value;
+	let num = document.getElementById("numForm").value;
+	let type = document.getElementById("typeForm").value;
+	let hour = document.getElementById("hourForm").value;
+  
+	if (name === "" || telephone === "" || email === "" || num === "" || type === "" || hour === ""){
+		swal({
+			title: "Empty fields.",
+			text: "All fields in the form must be filled.",
+			icon: "error",
+			button: "Close"
+		});
+   }else {
+		swal({
+			title: "Successful booking!",
+			text: "Your booking has been submitted successfully.",
+			icon: "success",
+			button: "Close"
+		});
+   }	
+	
+}
