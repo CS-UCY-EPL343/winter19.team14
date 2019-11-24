4644e9917796e22c43f149869be9b5075b80481e
nzacha <zicolas3@gmail.com>
2019-11-24 21:39:16 +0200

    worked on homepage and renamed some files

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function() {
   if (document.cookie.indexOf('visited=true') == -1){
     // load the overlay
-    $('#orderModal').modal({show:true});
-    
-    var year = 1000*60*60*24*1;
-    var expires = new Date((new Date()).valueOf() + year);
+    $('#orderModal').modal({show:true});    
+    var day = 1000*60*60*24*1;
+    var expires = new Date((new Date()).valueOf() + day);
     document.cookie = "visited=true;expires=" + expires.toUTCString();
-
   }
 }); 
 
