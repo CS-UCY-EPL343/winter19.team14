6aa476aaa5df22c0333ef3266e47172e347572cc
cachil01 <cachil01@cs.ucy.ac.cy>
2019-11-24 23:10:34 +0200

    Added a history tab and modified the change password

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,9 @@
 function getProfile(){
-
+	document.getElementById("profile-conf").checked = true;
 	document.getElementById("profile-name").value = "Marcos Antonios";
 	document.getElementById("profile-phone").value = "99421344";
 	document.getElementById("profile-address").value = "Kallipoleos 75, Nicosia 1678";
 	document.getElementById("profile-email").value = "user@ucy.ac.cy";
-	document.getElementById("profile-confirm").checked = true;
 	document.getElementById("profile-promo").checked = true;
 
 
@@ -41,5 +40,76 @@ function updateChanges() {
 		});
    }	
 	
+}
+
+var modal = document.getElementById("change_pass");
+
+var btn = document.getElementById("passBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+  
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
+  function changePassword(){
+	let old = document.getElementById("profile-old").value;
+	let password = document.getElementById("profile-pass").value;
+	let confirm = document.getElementById("profile-confirm").value;
+
+	if(old === "" || password === "" || confirm === ""){
+		swal({
+			title: "Not given a password.",
+			text: "Must give a password.",
+			icon: "error",
+			button: "Close"
+		});
+	} 
+	else if(password != confirm){
+		swal({
+			title: "Fail Change Password",
+			text: "Passwords do not match",
+			icon: "error",
+			button: "Close"
+		});
+	}
+	else{
+		document.getElementById("changepwd").reset();
+		modal.style.display = "none";
+		swal({
+			title: "Successful change!",
+			text: "Your password has been changed",
+			icon: "success",
+			button: "Close"
+		});
+	}
+
 }
 getProfile();
