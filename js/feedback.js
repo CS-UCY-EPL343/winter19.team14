681f3e935798fe39ccf1eedd8b12a6791f3cc0c9
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2019-11-26 00:20:59 +0200

    several changes in history tab, add order details modal, add swals for feedback submission and repeat order buttons

---
+++ js/feedback.js
@@ -0,0 +1,8 @@
+function feedbackSubmit() {
+    swal({
+        title: "Feedback successfully submitted",
+        text: "Thank you!",
+        icon: "success",
+        button: "Close"
+    });
+}
---
+++ js/repeatOrder.js
@@ -0,0 +1,8 @@
+function repeatOrder() {
+    swal({
+        title: "Order successfully placed",
+        // text: "Must give a password.",
+        icon: "success",
+        button: "Close"
+    });
+}
