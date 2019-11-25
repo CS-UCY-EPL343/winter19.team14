40dc00b43af1e861b55329cdf3ceec87e33f0dd2
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2019-11-26 00:41:06 +0200

    add swal message in buttons for order detail and add item

---
+++ js/admin.js
@@ -0,0 +1,17 @@
+function saveChanges() {
+    swal({
+        title: "Changes successfully saved",
+        text: "Order status updated",
+        icon: "success",
+        button: "Close"
+    });
+}
+
+function addItem() {
+    swal({
+        title: "Item successfully added",
+        // text: "Thank you!",
+        icon: "success",
+        button: "Close"
+    });
+}
